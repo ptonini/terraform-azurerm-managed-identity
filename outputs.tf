a795bd7a3179a3ef651f94110a5852662f11701a
ptonini <pedro.tonini@hotmail.com>
2024-01-18 20:54:22 -0300

    fix: update helm_release_values output

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "this" {
 output "helm_release_values" {
   value = {
     pod = { labels = { "azure.workload.identity/use" = "true" } }
-    serviceaccount = {
+    serviceAccount = {
       enabled     = true
       annotations = { "azure.workload.identity/client-id" = azurerm_user_assigned_identity.this.client_id }
     }
